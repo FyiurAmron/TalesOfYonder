86a343aa51ee013bfc0ba763ea0874f80761f80a
FyiurAmron <spamove@gmail.com>
2021-10-15 01:40:32 +0200

    trailing commas added

--- TalesOfYonder/src/LegacyEngine/Const.cs
+++ TalesOfYonder/src/LegacyEngine/Const.cs
@@ -7,7 +7,7 @@ public class Const {
     public static readonly string[] paletteFilenames = {
         DEFAULT_PALETTE_NAME,
         "logo.pal",
-        "webfoot.pal"
+        "webfoot.pal",
     };
 
     public static readonly PictureGroupDescriptor[] yt2PictureGroupDescriptors = {
@@ -18,64 +18,64 @@ public class Const {
             picCount = 23,
             paletteSelector = new() {
                 [0] = "logo.pal",
-                [16] = "webfoot.pal"
-            }
+                [16] = "webfoot.pal",
+            },
         },
         new() {
             description = "large 'pane style' UI pics",
             picWidth = 210,
             picHeight = 105,
-            picCount = 101
+            picCount = 101,
             // 6 pics for anim of a guy entering a room?
         },
         new() {
             description = "'tall' enemies",
             picWidth = 140,
             picHeight = 155,
-            picCount = 215
+            picCount = 215,
         },
         new() {
             description = "'wide' enemies",
             picWidth = 190,
             picHeight = 110,
-            picCount = 162
+            picCount = 162,
         },
         new() {
             description = "ground textures",
             picWidth = 224,
             picHeight = 74,
-            picCount = 18
+            picCount = 18,
         },
         new() {
             description = "ceiling textures",
             picWidth = 224,
             picHeight = 62,
-            picCount = 12
+            picCount = 12,
         },
         new() {
             description = "PC paperdolls & misc vertical gfx",
             picWidth = 56,
             picHeight = 136,
-            picCount = 55
+            picCount = 55,
         },
         new() {
             description = "spell & shop icons, faces, paperdoll equips",
             picWidth = 32,
             picHeight = 32,
-            picCount = 270
+            picCount = 270,
         },
         new() {
             description = "UI & inventory icons",
             picWidth = 16,
             picHeight = 16,
-            picCount = 510
+            picCount = 510,
         },
         new() {
             description = "map & status icons + rings (inv/paperdoll)",
             picWidth = 8,
             picHeight = 8,
-            picCount = 576
-        }
+            picCount = 576,
+        },
     };
 }
 
