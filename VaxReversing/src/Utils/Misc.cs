360d715f82d20c0f960c8f704d7a50961b4b800f
FyiurAmron <spamove@gmail.com>
2021-10-21 17:30:03 +0200

    added stride-related consts to Misc

--- VaxReversing/src/Utils/Misc.cs
+++ VaxReversing/src/Utils/Misc.cs
@@ -3,6 +3,8 @@ namespace Vax.Reversing.Utils {
 using System.Text;
 
 public static class Misc {
+    public const uint BITS_PER_BYTE = 8;
+    public const uint STRIDE_BYTES_ROUNDING = 4; // as per the definition from BitmapData.Stride
     public static Encoding defaultEncoding { get; set; } = Encoding.ASCII;
 
     public static uint roundUp( uint n, uint multiple ) => ( n + ( multiple - 1 ) ) / multiple * multiple;
