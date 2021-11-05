96abe85488ef83a51c0dc8bcc546af32d19c4bbc
FyiurAmron <spamove@gmail.com>
2021-11-05 20:34:04 +0100

    fixed a dumb typo error

--- TalesOfYonder/src/LegacyEngine/Map/TileContainer.cs
+++ TalesOfYonder/src/LegacyEngine/Map/TileContainer.cs
@@ -2,15 +2,18 @@ namespace TalesOfYonder.LegacyEngine.Map {
 
 using System.Collections.Generic;
 
-public record TileContainer( int xCount, int yCount, IList<Tile> tiles ) {
-    // private WorldTile[] tiles;
-
+public record TileContainer(
+    int xCount,
+    int yCount,
+    IList<Tile> tiles
+ ) {
     public Tile this[ int x, int y ] {
-        get => tiles[y * yCount + x];
-        set => tiles[y * yCount + x] = value;
+        get => tiles[y * xCount + x];
+        set => tiles[y * xCount + x] = value;
     }
 
-    public TileContainer( int xCount, int yCount ) : this( xCount, yCount, new Tile[xCount * yCount] ) {
+    public TileContainer( int xCount, int yCount )
+        : this( xCount, yCount, new Tile[xCount * yCount] ) {
     }
 }
 
